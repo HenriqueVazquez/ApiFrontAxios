e261e6f2e32644b1b360cadd5f3bf2e8668d210e
Henrique Vazquez <henrique_pm_vazquez@hotmail.com>
2023-03-22 15:48:09 -0300

    POST com axios

--- main.js
+++ main.js
@@ -5,7 +5,24 @@ function getUsers() {
     .then(response => {
       apiResult.textContent = JSON.stringify(response.data);
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error));
 }
+
+function addNewUser(newUser) {
+  axios.post(url, newUser)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => console.error(error));
+}
+
+const newUser = {
+  name: "Karen Tatiane",
+  avatar: "https://thumbs.gfycat.com/FickleWaryBrocketdeer-size_restricted.gif",
+  city: "São Paulo"
+
+}
+
+//addNewUser(newUser);
 
 getUsers();
